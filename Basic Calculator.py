6f62bef1b14c8f12081c2c4be3be153dcebb00dc
PanBanan78 <oskar.matacz2001@gmail.com>
2016-09-23 11:46:01 +0100

    Making a basic calculator
    *Backend of calculator is done

---
+++ Basic Calculator.py
@@ -0,0 +1,25 @@
+__author__ = 'Oskar'
+
+# Basic calculator program
+
+
+class CalculatorBackend():
+    @staticmethod
+    def add(x, y):
+        print(x + y)
+
+    @staticmethod
+    def subtract(x, y):
+        print(x - y)
+
+    @staticmethod
+    def multiply(x, y):
+        print(x * y)
+
+    @staticmethod
+    def divide(x, y):
+        print(x / y)
+
+
+
+
