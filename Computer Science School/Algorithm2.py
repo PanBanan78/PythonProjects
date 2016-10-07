c8f4518383bcd714ce029cb30010d66ccf8a72e1
PanBanan78 <oskar.matacz2001@gmail.com>
2016-10-07 21:45:52 +0100

    Quick commit

--- Computer Science School/Algorithm2.py
+++ Computer Science School/Algorithm2.py
@@ -1,11 +1,26 @@
 __author__ = "Oskar Matacz"
 __version__ = "0.5"
-balance = int(50000)
+
+
+def read_file(file_name):
+    with open(file_name, "r") as file:
+        read = file.read()
+        read = int(read)
+        return read
+
+
+def write_file(file_name, value):
+    with open(file_name, "w") as file:
+        print(value, file=file)
+
+
+balance = read_file("bankBalance")
+print(balance)
 maxWithdraw = int(250)
 
 print("Welcome to the Bank of McAuley")
 print("______________________________")
-print("Your balance is: £", balance)
+print("Your balance is: £" + str(balance))
 print("\nPlease note the max withdraw amount is £250")
 
 while True:
@@ -23,25 +38,28 @@ withdrawAmountIntDiv20 = int(withdrawAmountInt / 20)
 withdrawAmountIntDiv10 = int(withdrawAmountInt / 10)
 modulo20Div10 = int(modulo20 / 10)
 
+
 if withdrawAmountInt > 250:
     print("You cannot take out more than £250 at a time...")
+elif withdrawAmountInt > balance:
+    print("You cannot go overdraft!")
 else:
-    if withdrawAmountInt > 250:
-        print("You can't take out more than £250 at a time.")
-    elif withdrawAmountInt <= 250:
-        if modulo20 == 0:
-            print("Withdrawing £" + str(withdrawAmountInt), "with", withdrawAmountIntDiv20, "£20 notes.")
-            newBalanceCalc = balance - withdrawAmountIntDiv20 * 20
+    if modulo20 == 0:
+        print("Withdrawing £" + str(withdrawAmountInt), "with", withdrawAmountIntDiv20, "£20 notes.")
+        newBalanceCalc = balance - withdrawAmountIntDiv20 * 20
+        print("______________________________")
+        print("New balance: £" + str(newBalanceCalc))
+        write_file("bankBalance", newBalanceCalc)
+    elif modulo20 != 0:
+        if modulo10 == 0:
+            print("Withdrawing £" + str(withdrawAmountInt), "with", withdrawAmountIntDiv10, "£10 notes.")
+            newBalanceCalc = balance - withdrawAmountIntDiv10 * 10
             print("______________________________")
-            print("New balance:", newBalanceCalc)
-        elif modulo20 != 0:
-            if modulo10 == 0:
-                print("Withdrawing £" + str(withdrawAmountInt), "with", withdrawAmountIntDiv10, "£10 notes.")
-                newBalanceCalc = balance - withdrawAmountIntDiv10 * 10
-                print("______________________________")
-                print("New balance:", newBalanceCalc)
-            elif modulo10 != 0:
-                print("Withdrawing £" + str(withdrawAmountInt), "with", withdrawAmountIntDiv20, "£20 notes and", modulo20Div10, "£10 notes.")
-                newBalanceCalc = balance - ((withdrawAmountIntDiv20 * 20) + (modulo20Div10 * 10))
-                print("______________________________")
-                print("New balance:", newBalanceCalc)
+            print("New balance: £" + str(newBalanceCalc))
+            write_file("bankBalance", newBalanceCalc)
+        elif modulo10 != 0:
+            print("Withdrawing £" + str(withdrawAmountInt), "with", withdrawAmountIntDiv20, "£20 notes and", modulo20Div10, "£10 notes.")
+            newBalanceCalc = balance - ((withdrawAmountIntDiv20 * 20) + (modulo20Div10 * 10))
+            print("______________________________")
+            print("New balance: £" + str(newBalanceCalc))
+            write_file("bankBalance", newBalanceCalc)
